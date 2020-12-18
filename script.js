7422ce0d6b612bf38e0266f5f231919432d1a27f
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-18 02:51:05 +0100

    pendu en cours de realisation

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
 // mes variables
 
-let motCacher =document.getElementById("mot");
-
+let motCacher = document.getElementById("mot");
+let historique = document.getElementById("proposition");
+let element = document.getElementsByTagName("input")[0];
+let testJoueur = document.getElementsByTagName("button")[0];
+let tableau = [];
+let tableauSoluce = [];
+let choix = "";
+let message = document.getElementById("message");
 
 // je deffini un tableau avec les mots a trouver
 
-let mots = ["valise", "tasse", "tableaux", "voiture", "ordinateur", "bureau", "pantalon", "jardin", "maison",
-            "cendrier", "lampe", "tomate", "raisin", "pizza", "formation", "informatique", "fauteuil",
-            "escalier", "telephone", "pomme", "furet", "chat", "chien", "poule", "couverture", "cigarette"];
+let mots = [
+    "valise", "tasse", "tableaux", "voiture", "ordinateur", "bureau", "pantalon", "jardin", "maison",
+    "cendrier", "lampe", "tomate", "raisin", "pizza", "formation", "informatique", "fauteuil",
+    "escalier", "telephone", "pomme", "furet", "chat", "chien", "poule", "couverture", "cigarette",
+    "souris", "ecran", "figurine", "boite", "nuages", "console", "casque", "balance", "chaisse"
+];
 
 //je selectionne un des mots aleatoirement
 
 let randomot = mots[Math.floor(Math.random() * mots.length)];
-console.log(randomot)
+console.log("le mot a trouver : " + randomot);
 
-//je trnsmet le mot random dans mon espace mot a trouver
+// je cache le mot ramdom par des underscores et recupere le choix du joueur
 
+testJoueur.addEventListener("click", function () {
+    choix = element.value;
+    historique.innerHTML += "jouer : " + choix.toLocaleUpperCase() + " / ";
+    verif();
 
-// je recupere la touches proposer par l' utilisateur
-
+})
 //je verifie si la lettre et dans le mot a trouver
 
-//si oui je l' affiche dans le mots si non envoie d' un message pour l 'utilisateur
+for (let i = 0; i < randomot.length; i++) {
+    tableau.push(randomot[i]);
+    let caractere = randomot[i];
+    motCacher.innerHTML += caractere.replace(randomot[i], "_ ");
+
+    function verif() {
+        if (tableau.includes(element.value)) {
+            afficheLettre();
+        } else if (choix === randomot) {
+            message.innerHTML = "GAGNER !!!";
+            message.style.color = "green";
+        } else {
+            message.innerHTML = "PERDU cherche encore !!";
+            message.style.color = "red";
+        }
+
+        //si oui je l' affiche dans le mots
+        function afficheLettre() {
+            if (tableau[i].innerHTML === element.value) {
+                tableau[i].style.visibility = "visible";
+                motCacher.innerHTML = choix;
+            }
+        }
+    }
+}
+
+
+
