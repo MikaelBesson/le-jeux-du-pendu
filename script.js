5b0ede1a0665d6dc47ed925d39d37f3ad6811035
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-19 13:11:12 +0100

    jeux du pendu presque fini

--- script.js
+++ script.js
@@ -1,63 +1,59 @@
-// mes variables
-
-let motCacher = document.getElementById("mot");
-let historique = document.getElementById("proposition");
-let element = document.getElementsByTagName("input")[0];
-let testJoueur = document.getElementsByTagName("button")[0];
-let tableau = [];
-let tableauSoluce = [];
-let choix = "";
-let message = document.getElementById("message");
-
-// je deffini un tableau avec les mots a trouver
-
 let mots = [
-    "valise", "tasse", "tableaux", "voiture", "ordinateur", "bureau", "pantalon", "jardin", "maison",
-    "cendrier", "lampe", "tomate", "raisin", "pizza", "formation", "informatique", "fauteuil",
-    "escalier", "telephone", "pomme", "furet", "chat", "chien", "poule", "couverture", "cigarette",
-    "souris", "ecran", "figurine", "boite", "nuages", "console", "casque", "balance", "chaisse"
-];
+    "chaise", "feuille", "banane", "camion", "chaussure", "gendarme", "souris",
+    "ascenseur", "brouette", "chemise", "valise", "tasse", "tableaux", "voiture", "ordinateur",
+    "bureau", "pantalon", "jardin", "maison", "cendrier", "lampe", "tomate", "raisin", "pizza",
+    "formation", "informatique", "fauteuil", "escalier", "telephone", "pomme", "furet", "chat",
+    "chien", "poule", "couverture", "cigarette", "souris", "ecran", "figurine", "boite", "nuages",
+    "console", "casque", "balance",];
+let input = document.getElementById("input");
+let buttonVerify = document.getElementById("letterTry");
+let divEssai = document.getElementById("essai");
+let motRandom = mots[Math.floor(Math.random()*mots.length)];
+let guessWord = document.getElementById("guessWord");
+let answerTableau = [];
+let historique = document.getElementById("proposition");
+let essais = 6;
+let messageJoueur = document.getElementById("message");
+let img = document.getElementById('img');
+img.src = "images/imgPrincipal.jpg";
 
-//je selectionne un des mots aleatoirement
+divEssai.innerHTML = "essais restant " + essais;
+console.log(motRandom);
 
-let randomot = mots[Math.floor(Math.random() * mots.length)];
-console.log("le mot a trouver : " + randomot);
-
-// je cache le mot ramdom par des underscores et recupere le choix du joueur
-
-testJoueur.addEventListener("click", function () {
-    choix = element.value;
-    historique.innerHTML += "jouer : " + choix.toLocaleUpperCase() + " / ";
-    verif();
-
-})
-//je verifie si la lettre et dans le mot a trouver
-
-for (let i = 0; i < randomot.length; i++) {
-    tableau.push(randomot[i]);
-    let caractere = randomot[i];
-    motCacher.innerHTML += caractere.replace(randomot[i], "_ ");
-
-    function verif() {
-        if (tableau.includes(element.value)) {
-            afficheLettre();
-        } else if (choix === randomot) {
-            message.innerHTML = "GAGNER !!!";
-            message.style.color = "green";
-        } else {
-            message.innerHTML = "PERDU cherche encore !!";
-            message.style.color = "red";
-        }
-
-        //si oui je l' affiche dans le mots
-        function afficheLettre() {
-            if (tableau[i].innerHTML === element.value) {
-                tableau[i].style.visibility = "visible";
-                motCacher.innerHTML = choix;
-            }
-        }
-    }
+for (let i = 0; i < motRandom.length; i++) {
+    answerTableau[i] = "_";
+    guessWord.innerHTML = answerTableau.join(" ");
 }
 
+buttonVerify.addEventListener("click", function () {
+    let letter = input.value;
+    let found = true;
+    historique.innerHTML += "deja jouer : "+letter+ " / ";
 
 
+    for (let i = 0; i < motRandom.length; i++) {
+        if (motRandom[i] === letter) {
+            answerTableau[i] = letter;
+            guessWord.innerHTML = answerTableau.join(" ");
+        }
+        else if (!motRandom.includes(letter)) {
+            found = false;
+        }
+    }
+    if (!found) {
+        essais--;
+        divEssai.innerHTML = "essais restant " + essais;
+        img.src += 'images/img'+ essais +'.jpg';
+    }
+    if (essais <= 0) {
+        messageJoueur.innerHTML ="Perdu";
+        messageJoueur.style.color = "red";
+        alert("Le mot était : " + motRandom);
+
+    }
+    if (!answerTableau.includes("_")) {
+        messageJoueur.innerHTML ="Gagné !";
+        messageJoueur.style.color = "green";
+
+    }
+})
